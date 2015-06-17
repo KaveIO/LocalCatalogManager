9cf76f31f6360fe506356b6bc20e40448d25402e
Pavel Jez <pjez@kpmg.com>
2015-06-17 13:37:15 +0200

    test updated to check if parsing mechanism works

--- lcm/src/test/java/nl/kpmg/lcm/server/data/BackendFileTest.java
+++ lcm/src/test/java/nl/kpmg/lcm/server/data/BackendFileTest.java
@@ -18,6 +18,8 @@ package nl.kpmg.lcm.server.data;
 
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.Before;
@@ -51,6 +53,16 @@ public class BackendFileTest {
         BackendFileImpl testBackend = new BackendFileImpl(testDir);
         String testSchema =  testBackend.getSupportedUriSchema();
         assertEquals("file",testSchema);
+    }
+    
+    @Test
+    public void testParseUri() throws BackendException,IOException {
+        File testDir = new File(TEST_STORAGE_PATH);
+        String uri = "file://"+testDir.getCanonicalPath()+"/temp.csv";
+        BackendFileImpl testBackend = new BackendFileImpl(testDir);
+        URI dataUri = testBackend.parseUri(uri);
+        String filePath = dataUri.getPath();
+        assertEquals(testDir.getCanonicalPath()+"/temp.csv",filePath);
     }
     
      @Test
