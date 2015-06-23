5d3919c57b9243b4bed9a118d92cd8988aa37dbf
Pavel Jez <pjez@kpmg.com>
2015-06-23 16:18:01 +0200

    Implemented style for the whole class

--- lcm/src/test/java/nl/kpmg/lcm/server/data/BackendFileTest.java
+++ lcm/src/test/java/nl/kpmg/lcm/server/data/BackendFileTest.java
@@ -44,127 +44,183 @@ import com.google.common.io.Files;
 
 
 public class BackendFileTest {
-    
+
+    /**
+     * Temorary directory in which all the test files will exist.
+     */
     private static final String TEST_STORAGE_PATH = "temp_test/";
-    
+
+    /**
+     * Makes a temporary test directory.
+     * @throws Exception if it is not possible to make a test directory.
+     */
     @Before
-    public void setUp() throws Exception {
+    public final void setUp() throws Exception {
         // make test temp dir and set storage path
         File testDir = new File(TEST_STORAGE_PATH);
         testDir.mkdir();
     }
-    
+
+    /**
+     * Deletes the temporary test directory and its content, assuming there are
+     *  no subdirectories.
+     */
     @After
-    public void tearDown() {
+    public final void tearDown() {
         File file = new File(TEST_STORAGE_PATH);
-         for (File c : file.listFiles())
+         for (File c : file.listFiles()) {
             c.delete();
+         }
         file.delete();
     }
-    
+
+    /**
+     * Test to check if "file" URI scheme is supported by getSupportedUriSchema()
+     * method.
+     */
     @Test
-    public void testGetSupportedUriSchema(){
+    public final void testGetSupportedUriSchema() {
         File testDir = new File(TEST_STORAGE_PATH);
         BackendFileImpl testBackend = new BackendFileImpl(testDir);
         String testSchema =  testBackend.getSupportedUriSchema();
-        assertEquals("file",testSchema);
+        assertEquals("file", testSchema);
     }
-    
+
+    /**
+     * Tests if the URI is parsed correctly in {@link BackendFileImpl} class.
+     * 
+     * @throws BackendException if it is not possible to parse the URI
+     * @throws IOException if it is not possible to get full canonical path of a storage location.
+     */
     @Test
-    public void testParseUri() throws BackendException,IOException {
+    public final void testParseUri() throws BackendException, IOException {
         File testDir = new File(TEST_STORAGE_PATH);
-        String uri = "file://"+testDir.getCanonicalPath()+"/temp.csv";
-        BackendFileImpl testBackend = new BackendFileImpl(testDir); 
+        String uri = "file://" + testDir.getCanonicalPath() + "/temp.csv";
+        BackendFileImpl testBackend = new BackendFileImpl(testDir);
         URI dataUri = testBackend.parseUri(uri);
         String filePath = dataUri.getPath();
-        assertEquals(testDir.getCanonicalPath()+"/temp.csv",filePath);
+        assertEquals(testDir.getCanonicalPath() + "/temp.csv", filePath);
     }
-    
-    @Test
-    public void testGatherDatasetInformationEmptyMetadata() throws BackendException {
+
+    /**
+     * Tests what happens if {@link BackendFileImp} gathers information using empty
+     * {@link MetaData} object. Exception is expected.
+     *
+     * @throws BackendException if empty metadata are supplied.
+     */
+    @Test(expected = BackendException.class)
+    public final void testGatherDatasetInformationEmptyMetadata() throws BackendException {
         MetaData metaData = new MetaData();
         File testDir = new File(TEST_STORAGE_PATH);
         BackendFileImpl testBackend = new BackendFileImpl(testDir);
         DataSetInformation dataSetInformation = testBackend.gatherDataSetInformation(metaData);
     }
-    
-    @Test(expected=BackendException.class)
-    public void testGatherDatasetInformationWrongMetadata() throws BackendException {
+
+    /**
+     * Tests what happens if {@link BackendFileImp} gathers information using
+     * {@link MetaData} object with invalid URI. Exception is expected.
+     * 
+     * @throws BackendException if invalid URI is supplied in metadata
+     */
+    @Test(expected = BackendException.class)
+    public final void testGatherDatasetInformationWrongMetadata() throws BackendException {
         MetaData metaData = new MetaData();
         final String fileUri = "NotAnUri";
-        metaData.put("data", new HashMap() {{ put("uri", fileUri); }});
+        metaData.put("data", new HashMap() { { put("uri", fileUri); } });
         File testDir = new File(TEST_STORAGE_PATH);
         BackendFileImpl testBackend = new BackendFileImpl(testDir);
         DataSetInformation dataSetInformation = testBackend.gatherDataSetInformation(metaData);
     }
-    
+
+    /**
+     * Tests what happens if {@link BackendFileImp} gathers information using
+     * {@link MetaData} object with valid URI pointing to non-existing location.
+     * The {@link DataSetInformation} object should has isAttached() method equal to false.
+     * 
+     * @throws BackendException if it is not possible to gather information about the
+     *         dataset
+     * @throws IOException if it is not possible to get path of the storage directory
+     */
     @Test
-    public void testGatherDatasetInformationWrongLink() throws BackendException,IOException {
+    public final void testGatherDatasetInformationWrongLink() throws BackendException, IOException {
         MetaData metaData = new MetaData();
         File testDir = new File(TEST_STORAGE_PATH);
         // need to make sure that test file does not exist
-        File testFile = new File(TEST_STORAGE_PATH+"/temp.csv");
-        testFile.delete(); 
-        final String fileUri = "file://"+testDir.getCanonicalPath()+"/temp.csv";
-        metaData.put("data", new HashMap() {{ put("uri", fileUri); }});
+        File testFile = new File(TEST_STORAGE_PATH + "/temp.csv");
+        testFile.delete();
+        final String fileUri = "file://" + testDir.getCanonicalPath() + "/temp.csv";
+        metaData.put("data", new HashMap() { { put("uri", fileUri); } });
         BackendFileImpl testBackend = new BackendFileImpl(testDir);
         DataSetInformation dataSetInformation = testBackend.gatherDataSetInformation(metaData);
-        assertEquals(dataSetInformation.isAttached(),false);
+        assertEquals(dataSetInformation.isAttached(), false);
     }
-    
+
+    /**
+     * Tests what happens if {@link BackendFileImp} gathers information using
+     * {@link MetaData} object with valid URI pointing to existing location.
+     * The {@link DataSetInformation} object should has isAttached() method equal to true.
+     * 
+     * @throws BackendException if it is not possible to gather information about the
+     *         dataset
+     * @throws IOException if it is not possible to get path of the storage directory
+     */
     @Test
-    public void testGatherDatasetInformation() throws BackendException,IOException {
+    public final void testGatherDatasetInformation() throws BackendException, IOException {
         MetaData metaData = new MetaData();
         File testDir = new File(TEST_STORAGE_PATH);
-        File testFile = new File(TEST_STORAGE_PATH+"/temp.csv");
-        if (!testFile.exists())
+        File testFile = new File(TEST_STORAGE_PATH + "/temp.csv");
+        if (!testFile.exists()) {
             new FileOutputStream(testFile).close();
-        Date exp_timestamp = new Date(testFile.lastModified());
-        final String fileUri = "file://"+testDir.getCanonicalPath()+"/temp.csv";
-        metaData.put("data", new HashMap() {{ put("uri", fileUri); }});
+        }
+        Date expTimestamp = new Date(testFile.lastModified());
+        final String fileUri = "file://" + testDir.getCanonicalPath() + "/temp.csv";
+        metaData.put("data", new HashMap() { { put("uri", fileUri); } });
         BackendFileImpl testBackend = new BackendFileImpl(testDir);
         DataSetInformation dataSetInformation = testBackend.gatherDataSetInformation(metaData);
-        assertEquals(dataSetInformation.getModificationTime() ,exp_timestamp);
+        assertEquals(dataSetInformation.getModificationTime(), expTimestamp);
     }
-    
+
+    /**
+     * Tests store() method of {@link BackendFileImp}.
+     * Test creates a text file, then tries to store it and finally tests if it
+     * is identical using md5.
+     * 
+     * @throws BackendException if it is not possible to gather information about the
+     *         dataset
+     * @throws IOException if it is not possible to get path of the storage directory
+     */
     @Test
-    public void testStore() throws IOException, BackendException {
+    public final void testStore() throws IOException, BackendException {
         //first make a test file with some content
-        File testFile = new File(TEST_STORAGE_PATH+"/testFile.csv");
+        File testFile = new File(TEST_STORAGE_PATH + "/testFile.csv");
         testFile.createNewFile();
-        FileWriter writer = new FileWriter(testFile);
-        for(int i =0; i  < 10; i++){
-            writer.write("qwertyuiop");
-            writer.write("\n");
-            writer.write("asdfghjkl");
-            writer.write("\n");
-            writer.write("zxcvbnm,!@#$%^&*()_");
-            writer.write("\n");
-            writer.write("1234567890[][;',.");
-            writer.write("\n");
+        try (FileWriter writer = new FileWriter(testFile)) {
+            final int nLoops = 10;
+            for (int i = 0; i  < nLoops; i++) {
+                writer.write("qwertyuiop");
+                writer.write("\n");
+                writer.write("asdfghjkl");
+                writer.write("\n");
+                writer.write("zxcvbnm,!@#$%^&*()_");
+                writer.write("\n");
+                writer.write("1234567890[][;',.");
+                writer.write("\n");
+            }
+            writer.flush();
         }
-        writer.flush();
-        writer.close();
         // now make a metadata with uri
         File testDir = new File(TEST_STORAGE_PATH);
-        final String fileUri = "file://"+testDir.getCanonicalPath()+"/testStore.csv";
+        final String fileUri = "file://" + testDir.getCanonicalPath() + "/testStore.csv";
         MetaData metaData = new MetaData();
-        metaData.put("data", new HashMap() {{ put("uri", fileUri); }});
+        metaData.put("data", new HashMap() { { put("uri", fileUri); } });
         InputStream is = new FileInputStream(testFile);
         BackendFileImpl testBackend = new BackendFileImpl(testDir);
         testBackend.store(metaData, is);
         final File expected = testFile;
-        final File output = new File(testDir.getCanonicalPath()+"/testStore.csv");
-        HashCode hc_exp = Files.hash(expected, Hashing.md5());
-        HashCode hc_out = Files.hash(output, Hashing.md5());
-        assertEquals(hc_exp.toString(),hc_out.toString());
-        // missing assertEquals for files!
+        final File output = new File(testDir.getCanonicalPath() + "/testStore.csv");
+        HashCode hcExp = Files.hash(expected, Hashing.md5());
+        HashCode hcOut = Files.hash(output, Hashing.md5());
+        assertEquals(hcExp.toString(), hcOut.toString());
     }
-    
-     @Test
-  public void evaluatesExpression() {
-    int sum = 1+2+3;
-    assertEquals(6, sum);
-  }
 }
 
