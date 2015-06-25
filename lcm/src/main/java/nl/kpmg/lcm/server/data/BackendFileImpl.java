452591293f220c9388907d8b029a651add27dcab
Pavel Jez <pjez@kpmg.com>
2015-06-25 13:24:49 +0200

    Removed unneeded imports and updated javadoc

--- lcm/src/main/java/nl/kpmg/lcm/server/data/BackendFileImpl.java
+++ lcm/src/main/java/nl/kpmg/lcm/server/data/BackendFileImpl.java
@@ -22,7 +22,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PipedOutputStream;
 import java.net.URI;
 import java.util.Date;
 import java.util.logging.Level;
@@ -137,8 +136,9 @@ public class BackendFileImpl extends AbstractBackend {
     }
 
     /**
-     * Returns an output stream with a content of a file that is specified by
-     * metadata argument. {@link MetaData} needs to contain valid URI of a file.
+     * Returns an input stream with a content of a file that is specified by
+     * metadata argument. Returns null if it is not possible to open the file.
+     * {@link MetaData} needs to contain valid URI of a file.
      *
      * @param metadata MetaData with URI of the data
      * @return OutputStream with the data file content
@@ -151,26 +151,14 @@ public class BackendFileImpl extends AbstractBackend {
             throw new BackendException("No dataset attached.");
         }
         File file = getPathFromUri(metadata.getDataUri());
-       // OutputStream os = new PipedOutputStream();
-        try{
+        try {
             InputStream is = new FileInputStream(file);
             return is;
         } catch (FileNotFoundException ex) {
             Logger.getLogger(BackendFileImpl.class.getName())
-              .log(Level.SEVERE, "Did not find file: " + metadata.getDataUri(), ex);
+              .log(Level.SEVERE, "Did not find file: " + metadata.getDataUri()
+                      + ". Returning null.", ex);
         }
-//        try (InputStream is2 = ) {
-//            // this works for files < 2 GB. Otherwise the readBytes is -1.
-//            /** @TODO at the moment followin line throws NullPointerExpetion, please fix by providing suitable OutputStream*/
-//      //      int readBytes = IOUtils.copy(is, os);
-////            Logger.getLogger(BackendFileImpl.class.getName())
-////             .log(Level.INFO, "{0} bytes read", readBytes);
-//            return is2;
-//        } catch (IOException ex) {
-//             Logger.getLogger(BackendFileImpl.class.getName())
-//             .log(Level.SEVERE, "Couldn't read path: " + metadata.getDataUri(), ex);
-//        }
-//        is = null;
         return null;
     }
 
