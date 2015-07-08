47cea749ac3012fd004bc64a49502c8cd257d7f9
hoekstra.maarten@kpmg.nl <hoekstra.maarten@kpmg.nl>
2015-07-08 19:24:11 +0200

    Added alternative for NotFoundException to return 404 result

--- lcm/src/main/java/nl/kpmg/lcm/server/LoggingExceptionMapper.java
+++ lcm/src/main/java/nl/kpmg/lcm/server/LoggingExceptionMapper.java
@@ -17,27 +17,49 @@ package nl.kpmg.lcm.server;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 /**
+ * Catch all exception mapper for the REST interface.
+ *
+ * By default uncaught exceptions are not displayed by grizzly. This is nice
+ * but terrible for debugging. This mapper makes sure all uncaught exceptions
+ * are at least logged as a warning. This has a drawback that in some cases
+ * default behavior is lost (404 messages).
  *
  * @author mhoekstra
  */
 @Provider
 public class LoggingExceptionMapper  implements ExceptionMapper<Exception> {
+    /**
+     * The logger for this class.
+     */
     private static final Logger LOGGER = Logger.getLogger(LoggingExceptionMapper.class.getName());
 
+    /**
+     * Translates an exception to a response.
+     *
+     * @param ex the caught exception
+     * @return the response to provide.
+     */
     @Override
-    public Response toResponse(Exception ex) {
-        LOGGER.log(Level.WARNING, "Request failed", ex);
+    public final Response toResponse(final Exception ex) {
+        Response response;
+        if (NotFoundException.class.isAssignableFrom(ex.getClass())) {
+            response = Response.status(Response.Status.NOT_FOUND)
+                    .type("text/plain")
+                    .build();
+        } else {
+            LOGGER.log(Level.WARNING, "Request failed", ex);
+            response = Response.serverError()
+                    .entity("Request failed")
+                    .type("text/plain")
+                    .build();
+        }
 
-        // If debuging is on we could return: Exceptions.getStackTraceAsString(ex)
-        return Response
-                .serverError()
-                .entity("Request failed")
-                .type("text/plain")
-                .build();
+        return response;
     }
 }
