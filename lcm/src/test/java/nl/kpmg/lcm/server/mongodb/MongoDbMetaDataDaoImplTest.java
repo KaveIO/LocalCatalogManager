7961c8ff5a91ca7ca657d4025d24efab2087899f
Venkateswarlu Bommineni <venkateswarlub@kpmg.com>
2015-07-10 11:41:04 +0000

    Added Jmock and Mockito dependencies in pom.xml

---
+++ lcm/src/test/java/nl/kpmg/lcm/server/mongodb/MongoDbMetaDataDaoImplTest.java
@@ -0,0 +1,39 @@
+package nl.kpmg.lcm.server.mongodb;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import nl.kpmg.lcm.server.data.MetaData;
+import nl.kpmg.lcm.server.data.dao.MetaDataDao;
+import nl.kpmg.lcm.server.data.dao.file.MetaDataDaoImpl;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MongoDbMetaDataDaoImplTest { 
+	@Mock
+	MetaDataDao metaDataDao;
+			
+	@Before
+	public void setUp(){
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void testGetByName() {
+		metaDataDao = mock(MetaDataDaoImpl.class);
+		
+		MetaData md = new MetaData();
+		md.put("user", "abc");
+		
+		when(metaDataDao.getByName("user")).thenReturn(md);
+		MetaData md1 = metaDataDao.getByName("user");
+		assertEquals(md1.get("user").toString(),md.get("user"));				
+	}
+
+}
