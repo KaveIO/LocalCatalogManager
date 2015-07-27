f8b7b152223448197a3dc8dfabbc9a48168095c9
Venkateswarlu Bommineni <venkateswarlub@kpmg.com>
2015-07-27 10:45:04 +0000

    Added LCMBaseTest class to move common test fixture code

--- lcm/src/test/java/nl/kpmg/lcm/server/rest/ClientTest.java
+++ lcm/src/test/java/nl/kpmg/lcm/server/rest/ClientTest.java
@@ -1,77 +1,12 @@
 package nl.kpmg.lcm.server.rest;
 
-import java.io.File;
-
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-
-import nl.kpmg.lcm.server.Server;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"/application-context.xml","/application-context-file.xml"})
-public class ClientTest {
-    private static final String TEST_STORAGE_PATH = "test";
+import org.junit.Test;
 
-    private Server server;
-    private WebTarget target;
-
-    @BeforeClass
-    public static void setUpClass() {
-        File file;
-        file = new File(TEST_STORAGE_PATH);
-        file.mkdir();
-        file = new File(TEST_STORAGE_PATH + "/metadata");
-        file.mkdir();
-        file = new File(TEST_STORAGE_PATH + "/taskdescription");
-        file.mkdir();
-        file = new File(TEST_STORAGE_PATH + "/taskschedule");
-        file.mkdir();
-        file = new File(TEST_STORAGE_PATH + "/backend");
-        file.mkdir();
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-        File file;
-        file = new File(TEST_STORAGE_PATH);
-        file.delete();
-        file = new File(TEST_STORAGE_PATH + "/metadata");
-        file.delete();
-        file = new File(TEST_STORAGE_PATH + "/taskdescription");
-        file.delete();
-        file = new File(TEST_STORAGE_PATH + "/taskschedule");
-        file.delete();
-        file = new File(TEST_STORAGE_PATH + "/backend");
-        file.delete();
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        // start the server
-        server = new Server();
-        server.start();
-
-        // create the client
-        target = ClientBuilder.newClient().target(server.getBaseUri());
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        server.stop();
-    }
-
-    /**
+public class ClientTest extends LCMBaseTest {
+    
+	/**
      * Test to see that the client interface returns the interface versions.
      */
     @Test
