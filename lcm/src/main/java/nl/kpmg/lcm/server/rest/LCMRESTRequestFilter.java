2f126acae7a30bc4f16a4f2e29f2678e21e4eea6
hoekstra.maarten@kpmg.nl <hoekstra.maarten@kpmg.nl>
2015-08-21 11:11:31 +0200

    Used reflection to figure out the login path.

--- lcm/src/main/java/nl/kpmg/lcm/server/rest/LCMRESTRequestFilter.java
+++ lcm/src/main/java/nl/kpmg/lcm/server/rest/LCMRESTRequestFilter.java
@@ -8,85 +8,98 @@ import java.util.logging.Logger;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Link;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.ext.Provider;
 
 import nl.kpmg.lcm.server.AuthenticationManager;
+import nl.kpmg.lcm.server.rest.client.version0.UserController;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 
 @Provider
 @PreMatching
 public class LCMRESTRequestFilter implements ContainerRequestFilter {
 
-	
-	private static final Logger LOGGER = Logger.getLogger(LCMRESTRequestFilter.class.getName());
-	private AuthenticationManager am;
-		
-	@Autowired
-	public void setAuthenticationManager(AuthenticationManager am){
-		this.am = am;
-	}
-	/** 
-	 * Checks of Authorization Token for all URI's other than Login URI
-	 * @see javax.ws.rs.container.ContainerRequestFilter#filter(javax.ws.rs.container.ContainerRequestContext)
-	 */
-	@Override
-	public void filter(final ContainerRequestContext requestContext)
-			throws IOException {
-		requestContext.setSecurityContext(new LCMSecurityContext());		
-		
-		String path = requestContext.getUriInfo().getPath();
-		LOGGER.log(Level.INFO,"LCMRESTRequestFilter called with request path "+path);
-		if(requestContext.getRequest().getMethod().equals("OPTIONS")){
-			requestContext.abortWith(Response.status(Response.Status.OK).entity("You are not allowed to query OPTIONS.").build());
-			return;
-		}
-		
-		String serviceKey = requestContext.getUriInfo().getQueryParameters().getFirst("serviceKey");
-		
-		if(!am.isServiceKeyValid(serviceKey)){
-			requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("You are not Authorized to access LCM due to invalid service key "+serviceKey).build());
-			return;
-		}
-		
-		if(!path.startsWith("client/v0/users/login")){
-			String authorizationToken = requestContext.getUriInfo().getQueryParameters().getFirst("authorizationToken");	
-			LOGGER.log(Level.INFO,"LCMRESTRequestFilter called with request authorizationToken "+authorizationToken);
-			if(!am.isAuthorizationTokenValid(serviceKey, authorizationToken)){
-				requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("You are not Authorized to access LCM").build());
-			}
-		}
-	}	
-	
-	private final class LCMSecurityContext implements SecurityContext {
-		@Override
-		public Principal getUserPrincipal() {				
-			return new Principal(){
+    private static final Logger LOGGER = Logger.getLogger(LCMRESTRequestFilter.class.getName());
 
-				@Override
-				public String getName() {						
-					return "lcm";
-				}};
-		}
+    private final String loginPath;
 
-		@Override
-		public boolean isUserInRole(String role) {
-			LOGGER.log(Level.INFO,"LCMRESTRequestFilter::LCMSecurityContext called with role "+role);
-			return "administrator".equals(role);
-		}
+    private AuthenticationManager am;
 
-		@Override
-		public boolean isSecure() {				
-			return false;
-		}
+    public LCMRESTRequestFilter() {
+        // Reflect on the login method so we know what the actuall login path is.
+        loginPath = Link.fromMethod(UserController.class, "login").build().getUri().getPath();
+    }
 
-		@Override
-		public String getAuthenticationScheme() {				
-			return null;
-		}
-	}
-		
+    @Autowired
+    public void setAuthenticationManager(AuthenticationManager am) {
+        this.am = am;
+    }
+
+    /**
+     * Checks of Authorization Token for all URI's other than Login URI
+     *
+     * @see
+     * javax.ws.rs.container.ContainerRequestFilter#filter(javax.ws.rs.container.ContainerRequestContext)
+     */
+    @Override
+    public void filter(final ContainerRequestContext requestContext)
+            throws IOException {
+        requestContext.setSecurityContext(new LCMSecurityContext());
+
+        String path = requestContext.getUriInfo().getPath();
+        LOGGER.log(Level.INFO, "LCMRESTRequestFilter called with request path " + path);
+        if (requestContext.getRequest().getMethod().equals("OPTIONS")) {
+            requestContext.abortWith(Response.status(Response.Status.OK).entity("You are not allowed to query OPTIONS.").build());
+            return;
+        }
+
+        String serviceKey = requestContext.getUriInfo().getQueryParameters().getFirst("serviceKey");
+
+        if (!am.isServiceKeyValid(serviceKey)) {
+            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("You are not Authorized to access LCM due to invalid service key " + serviceKey).build());
+            return;
+        }
+
+        if (!path.startsWith(loginPath)) {
+            String authorizationToken = requestContext.getUriInfo().getQueryParameters().getFirst("authorizationToken");
+            LOGGER.log(Level.INFO, "LCMRESTRequestFilter called with request authorizationToken " + authorizationToken);
+            if (!am.isAuthorizationTokenValid(serviceKey, authorizationToken)) {
+                requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("You are not Authorized to access LCM").build());
+            }
+        }
+    }
+
+    private final class LCMSecurityContext implements SecurityContext {
+
+        @Override
+        public Principal getUserPrincipal() {
+            return new Principal() {
+
+                @Override
+                public String getName() {
+                    return "lcm";
+                }
+            };
+        }
+
+        @Override
+        public boolean isUserInRole(String role) {
+            LOGGER.log(Level.INFO, "LCMRESTRequestFilter::LCMSecurityContext called with role " + role);
+            return "administrator".equals(role);
+        }
+
+        @Override
+        public boolean isSecure() {
+            return false;
+        }
+
+        @Override
+        public String getAuthenticationScheme() {
+            return null;
+        }
+    }
+
 }
