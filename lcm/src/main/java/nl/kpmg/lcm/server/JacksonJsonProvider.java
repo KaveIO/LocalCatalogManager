9ed939b4b0c55c5cc4e701c806862912ae1fb179
hoekstra.maarten@kpmg.nl <hoekstra.maarten@kpmg.nl>
2015-09-01 17:27:35 +0200

    Faced some serious issues with the ObjectMapper. Half solved.

--- lcm/src/main/java/nl/kpmg/lcm/server/JacksonJsonProvider.java
+++ lcm/src/main/java/nl/kpmg/lcm/server/JacksonJsonProvider.java
@@ -29,14 +29,16 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
 /**
- * Jackson JSON processor could be controlled via providing a custom Jackson ObjectMapper instance.
- * This could be handy if you need to redefine the default Jackson behavior and to fine-tune how
- * your JSON data structures look like (copied from Jersey web site). *
+ * Jackson JSON processor could be controlled via providing a custom Jackson
+ * ObjectMapper instance. This could be handy if you need to redefine the
+ * default Jackson behavior and to fine-tune how your JSON data structures look
+ * like (copied from Jersey web site).
+ *
+ *
  * @see https://jersey.java.net/documentation/latest/media.html#d0e4799
  *
  *
@@ -45,6 +47,7 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 @Provider
 @Singleton
 public class JacksonJsonProvider implements ContextResolver<ObjectMapper> {
+
     /**
      * The Logger of this class.
      */
@@ -59,22 +62,26 @@ public class JacksonJsonProvider implements ContextResolver<ObjectMapper> {
      * so we can have somewhat clean json arriving at the client side.
      *
      * We disable FAIL_ON_UNKNOWN_PROPERTIES for the MetaData class. We try to
-     * safeguard freedom for any MetaData usage scenario. Therefor we allow fields
-     * to be persisted we don't know yet.
+     * safeguard freedom for any MetaData usage scenario. Therefor we allow
+     * fields to be persisted we don't know yet.
      *
      * Added a custom Link serializer to facilitate pretty HATEOAS links.
      */
     @SuppressWarnings("serial")
-    private static final ObjectMapper MAPPER = new ObjectMapper() { {
-        setSerializationInclusion(Include.NON_EMPTY);
-        disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        disable(SerializationFeature.FLUSH_AFTER_WRITE_VALUE);
-        // Add the linkSerializer. Used for pretty printing of hyperlinks in restful media
-        SimpleModule linkSerializer = new SimpleModule();
-        linkSerializer.addSerializer(Link.class, new LinkSerializer());
-        registerModule(linkSerializer);
-        setFilters(new NotFilteringFilterProvider());
-    } };
+    private static final ObjectMapper createMapper() {
+        return new ObjectMapper() {
+            {
+                setSerializationInclusion(Include.NON_EMPTY);
+                disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+                //disable(SerializationFeature.FLUSH_AFTER_WRITE_VALUE);
+                // Add the linkSerializer. Used for pretty printing of hyperlinks in restful media
+                SimpleModule linkSerializer = new SimpleModule();
+                linkSerializer.addSerializer(Link.class, new LinkSerializer());
+                registerModule(linkSerializer);
+                setFilters(new NotFilteringFilterProvider());
+            }
+        };
+    }
 
     /**
      * Default Constructor.
@@ -101,9 +108,7 @@ public class JacksonJsonProvider implements ContextResolver<ObjectMapper> {
     @Override
     public final ObjectMapper getContext(final Class<?> type) {
         LOGGER.log(Level.INFO, "MyJacksonProvider.getContext() called with type: {0}", type);
-
-        ObjectMapper objectMapperCopy = new ObjectMapper(MAPPER.getFactory().copy());
-        return objectMapperCopy;
+        return createMapper();
     }
 
     /**
@@ -125,4 +130,3 @@ public class JacksonJsonProvider implements ContextResolver<ObjectMapper> {
         }
     }
 }
-
