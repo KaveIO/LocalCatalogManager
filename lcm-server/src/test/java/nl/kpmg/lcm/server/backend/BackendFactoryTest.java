10e94fdb253823c9d444f52500062c7d9f162b6c
Stoyan Hristov <shristov@intracol.com>
2016-10-05 12:28:55 +0300

    Issue #3: add unit tests

---
+++ lcm-server/src/test/java/nl/kpmg/lcm/server/backend/BackendFactoryTest.java
@@ -0,0 +1,105 @@
+/*
+  * Copyright 2016 KPMG N.V. (unless otherwise stated).
+  * 
+  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+  *  in compliance with the License. You may obtain a copy of the License at
+  * 
+  *  http://www.apache.org/licenses/LICENSE-2.0
+  * 
+  *  Unless required by applicable law or agreed to in writing, software distributed under the License
+  *  is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+  *  or implied. See the License for the specific language governing permissions and limitations under
+  *  the License.
+ */
+package nl.kpmg.lcm.server.backend;
+
+import java.util.HashMap;
+import java.util.Map;
+import nl.kpmg.lcm.server.backend.exception.BackendNotImplementedException;
+import nl.kpmg.lcm.server.data.Storage;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * @author Stoyan Hristov<shristov@intracol.com>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {BackendFactory.class})
+public class BackendFactoryTest {
+
+    Storage csvStorage;
+    String csvSchame = "csv";
+    String csvStoragePath = "/tmp";
+    String csvStorageName = "csv-storage";
+
+    @Autowired
+    private BackendFactory backendFactory;
+
+    @Before
+    public void setUp() {
+        csvStorage = new Storage();
+        csvStorage.setName(csvStorageName);
+        Map options = new HashMap();
+        options.put("storagePath", csvStoragePath);
+        csvStorage.setOptions(options);
+    }
+
+    @Test
+    public void testCreateBackendCSV() throws Exception {
+        Backend csvBackend = backendFactory.createBackend(csvSchame, csvStorage);
+        assertNotNull(csvBackend);
+        assertTrue(csvBackend instanceof BackendCsvImpl);
+    }
+
+    @Test(expected = BackendNotImplementedException.class)
+    public void testCreateBackendInvalidSchema() throws Exception {
+        backendFactory.createBackend("this_is_invalid_schema", csvStorage);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateBackendNullSchema() throws Exception {
+        backendFactory.createBackend(null, csvStorage);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateBackendTooShortSchema() throws Exception {
+        backendFactory.createBackend("", csvStorage);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateBackendTooLongSchema() throws Exception {
+        String veryLongSchema = "111111111111111111111111111111111111111111"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff"
+                + "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb1111111111111111111dfsgsdffffff";
+
+        backendFactory.createBackend(veryLongSchema, csvStorage);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateBackendNullStorage() throws Exception {
+        backendFactory.createBackend(csvSchame, null);
+    }
+}
