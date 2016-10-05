ebf73628e9d367dee397dec366ad47f6f3561b52
hoekstra.maarten@kpmg.nl <hoekstra.maarten@kpmg.nl>
2016-10-05 18:05:08 +0200

    Made the serialization of the metadata object not include empty fields.

--- lcm-common/src/test/java/nl/kpmg/lcm/server/data/MetaDataTest.java
+++ lcm-common/src/test/java/nl/kpmg/lcm/server/data/MetaDataTest.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2015 KPMG N.V. (unless otherwise stated).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package nl.kpmg.lcm.server.data;
 
@@ -24,6 +22,9 @@ import java.util.Map;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.Test;
 
 /**
@@ -32,100 +33,125 @@ import org.junit.Test;
  */
 public class MetaDataTest {
 
-    private final Method setMethod;
+  private final Method setMethod;
 
-    private final Method getMethod;
+  private final Method getMethod;
 
-    public MetaDataTest() throws NoSuchMethodException {
-        setMethod = MetaData.class.getDeclaredMethod("set", String.class, Object.class);
-        setMethod.setAccessible(true);
+  public MetaDataTest() throws NoSuchMethodException {
+    setMethod = MetaData.class.getDeclaredMethod("set", String.class, Object.class);
+    setMethod.setAccessible(true);
 
-        getMethod = MetaData.class.getDeclaredMethod("get", String.class);
-        getMethod.setAccessible(true);
-    }
-
-
-    @Test
-    public void testGetReturnNullOnNoValue() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        String actual;
-
-        MetaData metaData = new MetaData();
-
-        actual = (String) getMethod.invoke(metaData, "notExistingKey");
-
-        assertNull(actual);
-    }
-
-    @Test
-    public void testGetReturnFirstLevelValue() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        String expected = "value";
-        String actual;
-
-        MetaData metaData = new MetaData();
-        metaData.set("firstLevelKey", expected);
-
-        actual = (String) getMethod.invoke(metaData, "firstLevelKey");
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testGetReturnSecondLevelValue() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        final String expected = "value";
-        String actual;
-
-        MetaData metaData = new MetaData();
-        metaData.set("firstLevelKey", new HashMap() {{ put("secondLevelKey", expected); }});
-
-        actual = (String) getMethod.invoke(metaData, "firstLevelKey.secondLevelKey");
-
-        assertEquals(expected, actual);
-    }
+    getMethod = MetaData.class.getDeclaredMethod("get", String.class);
+    getMethod.setAccessible(true);
+  }
 
 
+  @Test
+  public void testGetReturnNullOnNoValue()
+      throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    String actual;
 
-    @Test
-    public void testSetFirstLevelAttributes() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        String expected = "value";
-        String actual;
+    MetaData metaData = new MetaData();
 
-        MetaData metaData = new MetaData();
+    actual = (String) getMethod.invoke(metaData, "notExistingKey");
 
-        setMethod.invoke(metaData, "firstLevelKey", expected);
+    assertNull(actual);
+  }
 
-        actual = (String) metaData.get("firstLevelKey");
+  @Test
+  public void testGetReturnFirstLevelValue()
+      throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    String expected = "value";
+    String actual;
 
-        assertEquals(expected, actual);
-    }
+    MetaData metaData = new MetaData();
+    metaData.set("firstLevelKey", expected);
 
-    @Test
-    public void testSetSecondLevelAttributes() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        String expected = "value";
-        String actual;
+    actual = (String) getMethod.invoke(metaData, "firstLevelKey");
 
-        MetaData metaData = new MetaData();
+    assertEquals(expected, actual);
+  }
 
-        setMethod.invoke(metaData, "firstLevelKey.secondLevelKey", expected);
+  @Test
+  public void testGetReturnSecondLevelValue()
+      throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    final String expected = "value";
+    String actual;
 
-        actual = (String) ((Map) metaData.get("firstLevelKey")).get("secondLevelKey");
+    MetaData metaData = new MetaData();
+    metaData.set("firstLevelKey", new HashMap() {
+      {
+        put("secondLevelKey", expected);
+      }
+    });
 
-        assertEquals(expected, actual);
-    }
+    actual = (String) getMethod.invoke(metaData, "firstLevelKey.secondLevelKey");
 
-    @Test
-    public void testSetMulitpleSecondLevelAttributes() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        String expected1 = "value1";
-        String expected2 = "value2";
-        String actual;
+    assertEquals(expected, actual);
+  }
 
-        MetaData metaData = new MetaData();
 
-        setMethod.invoke(metaData, "firstLevelKey.secondLevelKey", expected1);
-        setMethod.invoke(metaData, "firstLevelKey.secondSecondLevelKey", expected2);
 
-        Object get = metaData.get("firstLevelKey");
+  @Test
+  public void testSetFirstLevelAttributes()
+      throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    String expected = "value";
+    String actual;
 
-        assertEquals(expected1, (String) ((Map) metaData.get("firstLevelKey")).get("secondLevelKey"));
-        assertEquals(expected2, (String) ((Map) metaData.get("firstLevelKey")).get("secondSecondLevelKey"));
-    }
+    MetaData metaData = new MetaData();
+
+    setMethod.invoke(metaData, "firstLevelKey", expected);
+
+    actual = (String) metaData.get("firstLevelKey");
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testSetSecondLevelAttributes()
+      throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    String expected = "value";
+    String actual;
+
+    MetaData metaData = new MetaData();
+
+    setMethod.invoke(metaData, "firstLevelKey.secondLevelKey", expected);
+
+    actual = (String) ((Map) metaData.get("firstLevelKey")).get("secondLevelKey");
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testSetMulitpleSecondLevelAttributes()
+      throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    String expected1 = "value1";
+    String expected2 = "value2";
+    String actual;
+
+    MetaData metaData = new MetaData();
+
+    setMethod.invoke(metaData, "firstLevelKey.secondLevelKey", expected1);
+    setMethod.invoke(metaData, "firstLevelKey.secondSecondLevelKey", expected2);
+
+    Object get = metaData.get("firstLevelKey");
+
+    assertEquals(expected1, (String) ((Map) metaData.get("firstLevelKey")).get("secondLevelKey"));
+    assertEquals(expected2,
+        (String) ((Map) metaData.get("firstLevelKey")).get("secondSecondLevelKey"));
+  }
+
+  @Test
+  public void testJacksonObjectMapperDoesntIncludeUnusedFields() throws JsonProcessingException {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    MetaData metaData = new MetaData();
+
+    metaData.setName("test");
+    metaData.setDataUri("file://local/bla.csv");
+
+    String expected = "{\"id\":null,\"name\":\"test\",\"data\":{\"uri\":\"file://local/bla.csv\"}}";
+    String actual = objectMapper.writeValueAsString(metaData);
+    assertEquals(expected, actual);
+  }
 }
