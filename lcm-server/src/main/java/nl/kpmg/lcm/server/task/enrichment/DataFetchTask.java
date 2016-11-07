5c4666a1d5a1c647626c911496831268d9c8ad4e
Stoyan Hristov <stoyan.hristov@bulpros.com>
2016-11-07 12:51:39 +0200

    issue #24:  use HttpClientFactory

--- lcm-server/src/main/java/nl/kpmg/lcm/server/task/enrichment/DataFetchTask.java
+++ lcm-server/src/main/java/nl/kpmg/lcm/server/task/enrichment/DataFetchTask.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2016 KPMG N.V. (unless otherwise stated).
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -14,13 +14,14 @@
 
 package nl.kpmg.lcm.server.task.enrichment;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
 import com.google.gson.stream.JsonReader;
 
+import nl.kpmg.lcm.client.HttpsClientFactory;
+import nl.kpmg.lcm.configuration.ClientConfiguration;
+import nl.kpmg.lcm.server.ServerException;
 import nl.kpmg.lcm.server.backend.Backend;
 import nl.kpmg.lcm.server.backend.BackendCsvImpl;
 import nl.kpmg.lcm.server.backend.Notification;
@@ -34,7 +35,6 @@ import nl.kpmg.lcm.server.task.EnrichmentTask;
 import nl.kpmg.lcm.server.task.TaskException;
 import nl.kpmg.lcm.server.task.TaskResult;
 
-import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -51,19 +51,22 @@ import java.util.logging.Logger;
  */
 public class DataFetchTask extends EnrichmentTask {
 
+  private static final Logger logger = Logger.getLogger(BackendCsvImpl.class.getName());
+
   @Autowired
   private StorageService storageService;
 
   @Autowired
   private RemoteLcmService remoteLcmService;
 
-  // TODO once the Authorization model is implemented this part have to be refactored
-  // Now directly is used admin user its password. After the reactoring there
-  //should be a user which is used only for remote calls
+  @Autowired
+  private ClientConfiguration configuration;
+
+  // TODO once the Authorization model is implemented this part may be refactored
+  // Now directly is used admin user and its password. After the refactoring there
+  // could be a user which is used only for remote calls
   private String adminUser;
   private String adminPassword;
-
-  private static final Logger logger = Logger.getLogger(BackendCsvImpl.class.getName());
 
   @Value("${lcm.server.adminUser}")
   public final void setAdminUser(final String adminUser) {
@@ -77,26 +80,44 @@ public class DataFetchTask extends EnrichmentTask {
 
   @Override
   protected TaskResult execute(MetaData metadata, Map options) throws TaskException {
+
     Notification validationNotification = new Notification();
     validation(options, validationNotification);
     if (validationNotification.hasErrors()) {
       throw new TaskException(validationNotification.errorMessage());
     }
 
-    String fetchUrl = getFetchURL(options);
+    String fetchUrl = buildFetchURL(options);
 
-    HttpAuthenticationFeature feature =
+    InputStream in = openInputStream(fetchUrl);
+
+    if (!writeData(in, metadata)) {
+      return TaskResult.FAILURE;
+    }
+
+    return TaskResult.SUCCESS;
+  }
+
+  private InputStream openInputStream(String fetchUrl) throws TaskException {
+    HttpAuthenticationFeature credentials =
         HttpAuthenticationFeature.basicBuilder().nonPreemptive()
             .credentials(adminUser, adminPassword).build();
 
-    ClientConfig clientConfig = new ClientConfig();
-    clientConfig.register(feature);
+    HttpsClientFactory clientFactory = new HttpsClientFactory(configuration, credentials);
 
-    Client client = ClientBuilder.newClient(clientConfig);
-    WebTarget target = client.target(fetchUrl);
+    WebTarget target = null;
+    try {
+      target = clientFactory.createWebTarget(fetchUrl);
+    } catch (ServerException ex) {
+      logger.log(Level.SEVERE, null, ex);
+      throw new TaskException(ex);
+    }
+
     Response response = target.request().get();
-    InputStream in = response.readEntity(InputStream.class);
+    return response.readEntity(InputStream.class);
+  }
 
+  private boolean writeData(InputStream in, MetaData metadata) {
     try {
       JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
       ContentIterator iterator = new JsonReaderContentIterator(reader);
@@ -107,10 +128,10 @@ public class DataFetchTask extends EnrichmentTask {
       metaDataService.update(metadata.getId(), metadata);
     } catch (Exception ex) {
       logger.log(Level.SEVERE, ex.getMessage());
-      return TaskResult.FAILURE;
-    }
 
-    return TaskResult.SUCCESS;
+      return false;
+    }
+    return true;
   }
 
   private void validation(Map options, Notification validationNotification) {
@@ -129,11 +150,12 @@ public class DataFetchTask extends EnrichmentTask {
     }
   }
 
-  private String getFetchURL(Map options) {
+  private String buildFetchURL(Map options) {
     String remoteLcmId = options.get("remoteLcm").toString();
     RemoteLcm remoteLcm = remoteLcmService.findOneById(remoteLcmId);
     String path = options.get("path").toString();
     String fetchUrl = remoteLcm.getUrl() + path;
+
     return fetchUrl;
   }
 
