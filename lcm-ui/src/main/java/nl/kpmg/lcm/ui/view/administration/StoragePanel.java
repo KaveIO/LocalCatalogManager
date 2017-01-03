d9500f90e88e107c196769caa5e4d4f3ae75bd59
Stoyan Hristov <shristov@intracol.com>
2017-01-03 15:58:26 +0200

    issue #37: licenses (#84)
    I37 licenses
     * add licenses where missing

--- lcm-ui/src/main/java/nl/kpmg/lcm/ui/view/administration/StoragePanel.java
+++ lcm-ui/src/main/java/nl/kpmg/lcm/ui/view/administration/StoragePanel.java
@@ -1,11 +1,17 @@
-/**
- * Copyright 2016 KPMG N.V.(unless otherwise stated).**Licensed under the Apache License,Version
- * 2.0(the"License");*you may not use this file except in compliance with the License.*You may
- * obtain a copy of the License at**http:// www.apache.org/licenses/LICENSE-2.0 Unless required by
- * applicable law or agreed to in writing,software*distributed under the License is distributed on
- * an"AS IS"BASIS,*WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.*See the
- * License for the specific language governing permissions and*limitations under the License.
+/*
+ * Copyright 2016 KPMG N.V. (unless otherwise stated).
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
+
 package nl.kpmg.lcm.ui.view.administration;
 
 import com.vaadin.ui.Button;
@@ -18,14 +24,13 @@ import com.vaadin.ui.VerticalLayout;
 
 import nl.kpmg.lcm.rest.types.StorageRepresentation;
 import nl.kpmg.lcm.rest.types.StoragesRepresentation;
+import nl.kpmg.lcm.server.data.Storage;
+import nl.kpmg.lcm.ui.component.DefinedLabel;
 
 import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.core.Link;
-
-import nl.kpmg.lcm.server.data.Storage;
-import nl.kpmg.lcm.ui.component.DefinedLabel;
 
 /****
  *
--- lcm-ui/src/main/java/nl/kpmg/lcm/ui/view/administration/TasksPanel.java
+++ lcm-ui/src/main/java/nl/kpmg/lcm/ui/view/administration/TasksPanel.java
@@ -1,14 +1,19 @@
-/**
- * Copyright 2016 KPMG N.V.(unless otherwise stated).**Licensed under the Apache License,Version
- * 2.0(the"License");*you may not use this file except in compliance with the License.*You may
- * obtain a copy of the License at**http:// www.apache.org/licenses/LICENSE-2.0 Unless required by
- * applicable law or agreed to in writing,software*distributed under the License is distributed on
- * an"AS IS"BASIS,*WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.*See the
- * License for the specific language governing permissions and*limitations under the License.
+/*
+ * Copyright 2016 KPMG N.V. (unless otherwise stated).
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
+
 package nl.kpmg.lcm.ui.view.administration;
 
-import com.vaadin.ui.AbstractComponent;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
@@ -18,12 +23,10 @@ import com.vaadin.ui.VerticalLayout;
 import nl.kpmg.lcm.rest.types.TaskDescriptionRepresentation;
 import nl.kpmg.lcm.rest.types.TaskDescriptionsRepresentation;
 import nl.kpmg.lcm.rest.types.TaskScheduleRepresentation;
-
-import java.util.Date;
-import java.util.List;
-
 import nl.kpmg.lcm.server.data.TaskDescription;
 import nl.kpmg.lcm.server.data.TaskSchedule;
+
+import java.util.Date;
 
 /****
  *
