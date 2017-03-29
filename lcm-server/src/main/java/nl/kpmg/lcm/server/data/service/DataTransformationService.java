784cea25b9d332c8de389b6eb2395a988aab1b0b
Stoyan Hristov <shristov@intracol.com>
2017-03-29 13:24:28 +0300

    issue #130 :  add data transformation service (#171)

---
+++ lcm-server/src/main/java/nl/kpmg/lcm/server/data/service/DataTransformationService.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 KPMG N.V. (unless otherwise stated).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package nl.kpmg.lcm.server.data.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author shristov
+ */
+@Service
+public class DataTransformationService {
+  private final Set<String> tabularDataFormats = new HashSet();
+
+  private final Set<String> fileDataFormats = new HashSet();
+
+  private final Set<String> itemDataFormats = new HashSet();
+
+  public DataTransformationService() {
+    tabularDataFormats.add("csv");
+    tabularDataFormats.add("hive");
+    tabularDataFormats.add("sql");
+
+    fileDataFormats.add("file");
+
+    itemDataFormats.add("json");
+    itemDataFormats.add("mongo");
+
+  }
+
+  public boolean isTransformationAdmissible(String inputDataFormat, String outputDataFormat) {
+    if (tabularDataFormats.contains(inputDataFormat)
+        && tabularDataFormats.contains(outputDataFormat)) {
+      return true;
+    }
+
+    if (fileDataFormats.contains(inputDataFormat) && fileDataFormats.contains(outputDataFormat)) {
+      return true;
+    }
+
+    if (itemDataFormats.contains(inputDataFormat) && itemDataFormats.contains(outputDataFormat)) {
+      return true;
+    }
+
+    if (fileDataFormats.contains(outputDataFormat)) {
+      return true;
+    }
+
+    return false;
+  }
+}
---
+++ lcm-server/src/test/java/nl/kpmg/lcm/server/data/service/DataTransformationServiceTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017 KPMG N.V. (unless otherwise stated).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package nl.kpmg.lcm.server.data.service;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author shristov
+ */
+public class DataTransformationServiceTest {
+
+  private static DataTransformationService service = new DataTransformationService();
+
+  @Test
+  public void testCsvToCsv() {
+    boolean result = service.isTransformationAdmissible("csv", "csv");
+    assertTrue(result);
+  }
+
+  @Test
+  public void testCsvToHive() {
+    boolean result = service.isTransformationAdmissible("csv", "hive");
+    assertTrue(result);
+  }
+
+  @Test
+  public void testHiveToHive() {
+    boolean result = service.isTransformationAdmissible("hive", "csv");
+    assertTrue(result);
+  }
+
+  @Test
+  public void testHiveToFile() {
+    boolean result = service.isTransformationAdmissible("hive", "file");
+    assertTrue(result);
+  }
+
+  @Test
+  public void testFileToHive() {
+    boolean result = service.isTransformationAdmissible("hive", "file");
+    assertTrue(result);
+  }
+}
