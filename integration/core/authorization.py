3c7c5057f25d00138fdc638d0c650086145fab1f
Nikolina Vancheva <nvancheva@intracol.com>
2017-11-22 16:06:28 +0200

    LCM434: removed try-except statement & reformatted setUpClass method:  - removed try-except statement from the authentication test plan;  - removed try-except statement from the authorization test plan;  - reformatted setUpClass method in the authorization test plan.

--- integration/core/authorization.py
+++ integration/core/authorization.py
@@ -10,7 +10,7 @@ from integration import IntegrationTestCase
 
 
 class TestAuhtorizationIntegration(IntegrationTestCase):
-    user_a_id =  None
+    user_a_id = None
     usergroup_a_id = None
     metadata_id = None
     metadata_path = None
@@ -20,137 +20,137 @@ class TestAuhtorizationIntegration(IntegrationTestCase):
     def setUpClass(cls):
         super(TestAuhtorizationIntegration, cls).setUpClass()
         cls.create_authorized_lcm()
+        cls.create_metadata()
+        cls.create_users()
+        cls.create_user_group()
 
     @classmethod
     def create_authorized_lcm(cls):
-        try:
-            result = cls.get_request('client/v0/authorizedlcm')
-            cls.assertTrue(cls, result.code == 200)
+        result = cls.get_request('client/v0/authorizedlcm')
+        cls.assertTrue(cls, result.code == 200)
 
-            items = cls.get_response_items(cls, result)
-            cls.assertIsNone(cls, items, 'No items are expected!')
+        items = cls.get_response_items(cls, result)
+        cls.assertIsNone(cls, items, 'No items are expected!')
 
-            result = cls.post_request("client/v0/authorizedlcm",
-                                      {"name": "integration-test-lcm", "uniqueId": cls.unique_lcm_id,
-                                       "applicationId": cls.application_id, "applicationKey": cls.application_key},
-                                      "application/nl.kpmg.lcm.server.data.AuthorizedLcm+json"
-                                      )
-            cls.assertTrue(cls, result.code == 200)
+        result = cls.post_request("client/v0/authorizedlcm",
+                                  {"name": "integration-test-lcm", "uniqueId": cls.unique_lcm_id,
+                                   "applicationId": cls.application_id, "applicationKey": cls.application_key},
+                                  "application/nl.kpmg.lcm.server.data.AuthorizedLcm+json"
+                                  )
+        cls.assertTrue(cls, result.code == 200)
 
-            cls.authorization = cls.get_authorization(cls, cls.application_id, cls.hashed_key)
+        cls.authorization = cls.get_authorization(cls, cls.application_id, cls.hashed_key)
 
-            result = cls.get_request('remote/v0/test', None,
-                                     "application/json",
-                                     cls.authorization,
-                                     cls.unique_lcm_id
-                                     )
-            cls.assertTrue(cls, result.code == 200)
+        result = cls.get_request('remote/v0/test', None,
+                                 "application/json",
+                                 cls.authorization,
+                                 cls.unique_lcm_id
+                                 )
+        cls.assertTrue(cls, result.code == 200)
 
-            result = cls.post_request('client/v0/local',
-                                      {"name": "metadataA",
-                                       "data" : { "uri" : [ "file://localFile/f.txt" ],
-                                                  "path" : "kpmg" }},
-                                      "application/nl.kpmg.lcm.server.data.MetaData+json")
-            cls.assertTrue(cls, result.code == 200)
+    @classmethod
+    def create_metadata(cls):
+        # create metadata A
+        result = cls.post_request('client/v0/local',
+                                  {"name": "metadataA",
+                                   "data": {"uri": ["file://localFile/f.txt"],
+                                            "path": "kpmg"}},
+                                  "application/nl.kpmg.lcm.server.data.MetaData+json")
+        cls.assertTrue(cls, result.code == 200)
 
-            # get metadataA`s id and path
-            result = cls.get_request('client/v0/local')
-            cls.assertTrue(cls, result.code == 200)
-            items = cls.get_response_items(cls, result)
-            cls.metadata_id = items[0]['item']['id']
-            cls.metadata_path = items[0]['item']['data']['path']
+        # get metadata A`s id and path
+        result = cls.get_request('client/v0/local')
+        cls.assertTrue(cls, result.code == 200)
+        items = cls.get_response_items(cls, result)
+        cls.metadata_id = items[0]['item']['id']
+        cls.metadata_path = items[0]['item']['data']['path']
 
-            result = cls.post_request("client/v0/users",
-                                      {"name": "userA", "origin": cls.unique_lcm_id,
-                                       "role": "remoteUser", "newPassword": "userA"},
-                                      "application/nl.kpmg.lcm.server.data.User+json"
-                                      )
-            cls.assertTrue(cls, result.code == 200)
+    @classmethod
+    def create_users(cls):
+        # create user A with remote user role
+        result = cls.post_request("client/v0/users",
+                                  {"name": "userA", "origin": cls.unique_lcm_id,
+                                   "role": "remoteUser", "newPassword": "userA"},
+                                  "application/nl.kpmg.lcm.server.data.User+json"
+                                  )
+        cls.assertTrue(cls, result.code == 200)
 
-            # get userA`s id
-            result = cls.get_request("client/v0/users")
-            cls.assertTrue(cls, result.code == 200)
-            items = cls.get_response_items(cls, result)
-            cls.user_a_id = items[0]['item']['id']
+        # get userA`s id
+        result = cls.get_request("client/v0/users")
+        cls.assertTrue(cls, result.code == 200)
+        items = cls.get_response_items(cls, result)
+        cls.user_a_id = items[0]['item']['id']
 
-            result = cls.post_request("client/v0/users",
-                                      {"name": "userB", "origin": cls.unique_lcm_id,
-                                       "role": "remoteUser", "newPassword": "userB"},
-                                      "application/nl.kpmg.lcm.server.data.User+json"
-                                      )
-            cls.assertTrue(cls, result.code == 200)
+        # create user B with remote user role
+        result = cls.post_request("client/v0/users",
+                                  {"name": "userB", "origin": cls.unique_lcm_id,
+                                   "role": "remoteUser", "newPassword": "userB"},
+                                  "application/nl.kpmg.lcm.server.data.User+json"
+                                  )
+        cls.assertTrue(cls, result.code == 200)
 
-            result = cls.post_request("client/v0/users",
-                                      {"name": "userC", "origin": 'local',
-                                       "role": "apiUser", "newPassword": "userC"},
-                                      "application/nl.kpmg.lcm.server.data.User+json"
-                                      )
-            cls.assertTrue(cls, result.code == 200)
+        # create user B with api user role
+        result = cls.post_request("client/v0/users",
+                                  {"name": "userC", "origin": 'local',
+                                   "role": "apiUser", "newPassword": "userC"},
+                                  "application/nl.kpmg.lcm.server.data.User+json"
+                                  )
+        cls.assertTrue(cls, result.code == 200)
 
-            result = cls.post_request("client/v0/userGroups",
-                                      {"name": "userGroupA", "users": [cls.user_a_id]},
-                                      'application/nl.kpmg.lcm.server.data.UserGroup+json'
-                                      )
-            cls.assertTrue(cls, result.code == 200)
+    @classmethod
+    def create_user_group(cls):
+        # create user group A
+        result = cls.post_request("client/v0/userGroups",
+                                  {"name": "userGroupA", "users": [cls.user_a_id]},
+                                  'application/nl.kpmg.lcm.server.data.UserGroup+json'
+                                  )
+        cls.assertTrue(cls, result.code == 200)
 
-            # get usergroupA`s id
-            result = cls.get_request("client/v0/userGroups")
-            cls.assertTrue(cls, result.code == 200)
-            items = cls.get_response_items(cls, result)
-            cls.usergroup_a_id = items[0]['item']['id']
-
-        except urllib.error.HTTPError as error:
-            cls.print_docker_output('lcm-integration-server')
+        # get user group A`s id
+        result = cls.get_request("client/v0/userGroups")
+        cls.assertTrue(cls, result.code == 200)
+        items = cls.get_response_items(cls, result)
+        cls.usergroup_a_id = items[0]['item']['id']
 
     @classmethod
     def tearDownClass(cls):
-        try:
-            result = cls.get_request('client/v0/authorizedlcm')
+        result = cls.get_request('client/v0/authorizedlcm')
+        cls.assertTrue(cls, result.code == 200)
+        items = cls.get_response_items(cls, result)
+        lcm_id = items[0]['item']['id']
+
+        # delete
+        result = cls.delete_request('client/v0/authorizedlcm/' + lcm_id)
+        cls.assertTrue(cls, result.code == 200)
+
+        # delete the metadata
+        result = cls.delete_request('client/v0/local/' + cls.metadata_id)
+        cls.assertTrue(cls, result.code == 200)
+
+        result = cls.get_request('client/v0/users')
+        cls.assertTrue(cls, result.code == 200)
+        items = cls.get_response_items(cls, result)
+        for user in items:
+            user_id = user['item']['id']
+            result = cls.delete_request('client/v0/users/' + user_id)
             cls.assertTrue(cls, result.code == 200)
-            items = cls.get_response_items(cls, result)
-            lcm_id = items[0]['item']['id']
-
-            # delete
-            result = cls.delete_request('client/v0/authorizedlcm/' + lcm_id)
-            cls.assertTrue(cls, result.code == 200)
-
-            # delete the metadata
-            result = cls.delete_request('client/v0/local/' + cls.metadata_id)
-            cls.assertTrue(cls, result.code == 200)
-
-            result = cls.get_request('client/v0/users')
-            cls.assertTrue(cls, result.code == 200)
-            items = cls.get_response_items(cls, result)
-            for user in items:
-                user_id = user['item']['id']
-                result = cls.delete_request('client/v0/users/' + user_id)
-                cls.assertTrue(cls, result.code == 200)
-
-        except urllib.error.HTTPError as error:
-            cls.print_docker_output('lcm-integration-server')
-
-        finally:
-            super(TestAuhtorizationIntegration, cls).tearDownClass()
-
+        super(TestAuhtorizationIntegration, cls).tearDownClass()
 
     def request_access_to_remote_metadata_using_remote_user(self, user):
-        return self.get_request('remote/v0/metadata/'+  self.metadata_id, data=None, content_type="application/json",
+        return self.get_request('remote/v0/metadata/' + self.metadata_id, data=None, content_type="application/json",
                                 authorization=self.authorization, origin=self.unique_lcm_id, remote_user=user)
 
-
     def update_user_removing_allowed_metadata_or_path(self):
-        return self.put_request('client/v0/users', { "id": self.user_a_id, "name": "userA",
-                                                     "origin": self.unique_lcm_id,
-                                                     "role": "remoteUser", "newPassword": "userA"},
+        return self.put_request('client/v0/users', {"id": self.user_a_id, "name": "userA",
+                                                    "origin": self.unique_lcm_id,
+                                                    "role": "remoteUser", "newPassword": "userA"},
                                 "application/nl.kpmg.lcm.server.data.User+json")
-
 
     def update_user_group_removing_allowed_metadata_or_path(self):
         return self.put_request('client/v0/userGroups', {"id": self.usergroup_a_id,
                                                          "name": "userGroupA",
                                                          "users": [self.user_a_id]},
                                 "application/nl.kpmg.lcm.server.data.UserGroup+json")
-
 
     def test_case1_administrator_role(self):
         result = self.get_request('remote/v0/metadata')
@@ -159,8 +159,8 @@ class TestAuhtorizationIntegration(IntegrationTestCase):
         self.assertTrue(result.code == 200)
 
     def test_case2_remote_user_role(self):
-        result = self.get_request('remote/v0/metadata',  None, "application/json",
-                                  self.authorization , self.unique_lcm_id, 'userA' )
+        result = self.get_request('remote/v0/metadata', None, "application/json",
+                                  self.authorization, self.unique_lcm_id, 'userA')
         self.assertTrue(result.code == 200)
 
         with self.assertRaises(Exception) as context:
@@ -176,20 +176,19 @@ class TestAuhtorizationIntegration(IntegrationTestCase):
         self.assertTrue(result.code == 200)
 
         with self.assertRaises(Exception) as context:
-            self.get_request('remote/v0/metadata', None ,
+            self.get_request('remote/v0/metadata', None,
                              "application/json", authorization)
         self.assertTrue('Forbidden' in context.exception.reason)
         self.assertTrue(context.exception.code == 403)
-
 
     def test_case4_remote_user_directly_athorized_by_id(self):
         result = self.get_request('client/v0/users/' + self.user_a_id)
         self.assertTrue(result.code == 200)
 
-        result = self.put_request('client/v0/users', { "id": self.user_a_id, "name": "userA",
-                                                       "origin": self.unique_lcm_id,
-                                                       "role": "remoteUser", "newPassword": "userA",
-                                                       "allowedMetadataList": [self.metadata_id]},
+        result = self.put_request('client/v0/users', {"id": self.user_a_id, "name": "userA",
+                                                      "origin": self.unique_lcm_id,
+                                                      "role": "remoteUser", "newPassword": "userA",
+                                                      "allowedMetadataList": [self.metadata_id]},
                                   "application/nl.kpmg.lcm.server.data.User+json")
         self.assertTrue(result.code == 200)
 
@@ -206,7 +205,6 @@ class TestAuhtorizationIntegration(IntegrationTestCase):
             self.request_access_to_remote_metadata_using_remote_user('userA')
         self.assertTrue('Forbidden' in context.exception.reason)
         self.assertTrue(context.exception.code == 403)
-
 
     def test_case5_remote_user_directly_athorized_by_path(self):
         result = self.get_request('client/v0/users/' + self.user_a_id)
@@ -233,7 +231,6 @@ class TestAuhtorizationIntegration(IntegrationTestCase):
         self.assertTrue('Forbidden' in context.exception.reason)
         self.assertTrue(context.exception.code == 403)
 
-
     def test_case6_remote_user_group_athorized_by_id(self):
         result = self.get_request('client/v0/userGroups/' + self.usergroup_a_id)
         self.assertTrue(result.code == 200)
@@ -241,7 +238,7 @@ class TestAuhtorizationIntegration(IntegrationTestCase):
         result = self.put_request('client/v0/userGroups', {"id": self.usergroup_a_id,
                                                            "name": "userGroupA",
                                                            "users": [self.user_a_id],
-                                                           "allowedMetadataList" : [self.metadata_id]},
+                                                           "allowedMetadataList": [self.metadata_id]},
                                   "application/nl.kpmg.lcm.server.data.UserGroup+json")
         self.assertTrue(result.code == 200)
 
@@ -264,7 +261,6 @@ class TestAuhtorizationIntegration(IntegrationTestCase):
         self.assertTrue('Forbidden' in context.exception.reason)
         self.assertTrue(context.exception.code == 403)
 
-
     def test_case7_remote_user_group_athorized_by_path(self):
         result = self.get_request('client/v0/userGroups/' + self.usergroup_a_id)
         self.assertTrue(result.code == 200)
@@ -272,7 +268,7 @@ class TestAuhtorizationIntegration(IntegrationTestCase):
         result = self.put_request('client/v0/userGroups', {"id": self.usergroup_a_id,
                                                            "name": "userGroupA",
                                                            "users": [self.user_a_id],
-                                                           "allowedPathList" : [self.metadata_path]},
+                                                           "allowedPathList": [self.metadata_path]},
                                   "application/nl.kpmg.lcm.server.data.UserGroup+json")
         self.assertTrue(result.code == 200)
 
