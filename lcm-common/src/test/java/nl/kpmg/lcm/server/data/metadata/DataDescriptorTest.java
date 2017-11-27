917394992afa41537d273884af4237f96361db1a
Nikolina Vancheva <nvancheva@intracol.com>
2017-11-27 15:32:18 +0200

    LCM452: fixed metadata with invalid data uri & added unit tests  - fixed successful creating/editing of metadata with invalid data uri;  - added unit tests for metadata data uri validation.

--- lcm-common/src/test/java/nl/kpmg/lcm/server/data/metadata/DataDescriptorTest.java
+++ lcm-common/src/test/java/nl/kpmg/lcm/server/data/metadata/DataDescriptorTest.java
@@ -13,15 +13,14 @@
  */
 package nl.kpmg.lcm.server.data.metadata;
 
-import nl.kpmg.lcm.common.data.metadata.DataDescriptor;
-import nl.kpmg.lcm.common.data.metadata.MetaData;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import nl.kpmg.lcm.common.data.metadata.DataDescriptor;
+import nl.kpmg.lcm.common.data.metadata.MetaData;
 import nl.kpmg.lcm.common.validation.Notification;
 
 import org.junit.Test;
@@ -88,4 +87,134 @@ public class DataDescriptorTest {
     data.validate(notification);
     assertTrue(notification.hasErrors());
   }
+
+  @Test
+  public void testValidDataFormat() {
+    MetaData metaData = new MetaData();
+
+    String name = "mongoMetadata";
+    metaData.setName(name);
+
+    String uri = "mongo://mongoStorage/dir1/dir2/../table";
+    List uriList = new ArrayList();
+    uriList.add(uri);
+    String path = "kpmg2";
+
+    DataDescriptor data = new DataDescriptor(metaData);
+    data.setUri(uriList);
+    data.setPath(path);
+
+    Notification notification = new Notification();
+    data.validate(notification);
+    assertFalse(notification.hasErrors());
+  }
+
+
+  @Test
+  public void testValidDataItemExtension() {
+    MetaData metaData = new MetaData();
+
+    String name = "fileMetadata";
+    metaData.setName(name);
+
+    String uri = "file://fileStorage/file*.txt";
+    List uriList = new ArrayList();
+    uriList.add(uri);
+    String path = "kpmg2";
+
+    DataDescriptor data = new DataDescriptor(metaData);
+    data.setUri(uriList);
+    data.setPath(path);
+
+    Notification notification = new Notification();
+    data.validate(notification);
+    assertFalse(notification.hasErrors());
+  }
+
+  @Test
+  public void testInvalidDataFormat() {
+    MetaData metaData = new MetaData();
+
+    String name = "metadata";
+    metaData.setName(name);
+
+    String uri = "notExistingDataFormat://storage/dir1/dir2/../file";
+    List uriList = new ArrayList();
+    uriList.add(uri);
+    String path = "kpmg2";
+
+    DataDescriptor data = new DataDescriptor(metaData);
+    data.setUri(uriList);
+    data.setPath(path);
+
+    Notification notification = new Notification();
+    data.validate(notification);
+    assertTrue(notification.hasErrors());
+  }
+
+  @Test
+  public void testInvalidStorageName() {
+    MetaData metaData = new MetaData();
+
+    String name = "hiveMetadata";
+    metaData.setName(name);
+
+    String uri = "hive://hive Storage/dir1/dir2/../table";
+    List uriList = new ArrayList();
+    uriList.add(uri);
+    String path = "kpmg2";
+
+    DataDescriptor data = new DataDescriptor(metaData);
+    data.setUri(uriList);
+    data.setPath(path);
+
+    Notification notification = new Notification();
+    data.validate(notification);
+    assertTrue(notification.hasErrors());
+  }
+
+
+  @Test
+  public void testInvalidDataItemPath() {
+    MetaData metaData = new MetaData();
+
+    String name = "azurecsvMetadata";
+    metaData.setName(name);
+
+    String uri = "azurecsv://azureStorage/dir1$/dir2/../table";
+    List uriList = new ArrayList();
+    uriList.add(uri);
+    String path = "kpmg2";
+
+    DataDescriptor data = new DataDescriptor(metaData);
+    data.setUri(uriList);
+    data.setPath(path);
+
+    Notification notification = new Notification();
+    data.validate(notification);
+    assertTrue(notification.hasErrors());
+  }
+
+
+  @Test
+  public void testInvalidDataItemExtension() {
+    MetaData metaData = new MetaData();
+
+    String name = "hdfsFileMetadata";
+    metaData.setName(name);
+
+    String uri = "hdfsfile://hdfsFileStorage/file.txt*";
+    List uriList = new ArrayList();
+    uriList.add(uri);
+    String path = "kpmg2";
+
+    DataDescriptor data = new DataDescriptor(metaData);
+    data.setUri(uriList);
+    data.setPath(path);
+
+    Notification notification = new Notification();
+    data.validate(notification);
+    assertTrue(notification.hasErrors());
+  }
+
 }
