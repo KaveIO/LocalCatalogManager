d12a5ef2bdd645def4bd9a72a490ddaa66bddd0e
Nikolina Vancheva <nvancheva@intracol.com>
2018-01-05 12:32:31 +0200

    - removed hardcoded credentials for the default admin user; - fixed linking to the server certificate; - fixed script for generating ssl certificates.

--- dev-scripts/setup-ssl.sh
+++ dev-scripts/setup-ssl.sh
@@ -4,4 +4,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )
 
 mkdir -p $DIR/certificates
-$DIR/create-certificates.sh $DIR/certificates $DIR
+$DIR/create-certificates.sh $DIR/certificates
